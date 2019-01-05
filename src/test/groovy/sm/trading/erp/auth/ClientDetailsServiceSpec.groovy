bb8c8295dcdab281bc5f4f4bd027e6570a20effd
mainul35 <mainuls18@gmail.com>
2019-01-05 13:05:16 +0600

    committing

---
+++ src/test/groovy/sm/trading/erp/auth/ClientDetailsServiceSpec.groovy
@@ -0,0 +1,18 @@
+package sm.trading.erp.auth
+
+import grails.testing.services.ServiceUnitTest
+import spock.lang.Specification
+
+class ClientDetailsServiceSpec extends Specification implements ServiceUnitTest<ClientDetailsService>{
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
